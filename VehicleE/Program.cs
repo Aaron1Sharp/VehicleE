d37016014ee613e79c476b7f06c3f150cdd52d69
User <User@GL-424>
2020-01-22 17:56:29 +0600

    Добавьте файлы проекта.

---
+++ VehicleE/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class Transport
+    {
+        public string Mark { get; set; }
+        public string Number { get; set; }
+        public string Speed { get; set; }
+
+    }
+}
